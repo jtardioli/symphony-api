df6662c108dee8bfdf3ec52c1d6fed3d7db87783
jtardioli <joshua.tardioli@gmail.com>
2022-12-01 20:04:53 -0500

    major refactoring

--- src/controllers/releases.controller.ts
+++ src/controllers/releases.controller.ts
@@ -5,17 +5,26 @@ export const handleCreateRelease = async (req: Request, res: Response) => {
   try {
     const { release } = req.body;
 
-    const { mintEndDateTime, mintStartDateTime, title, releaseType } = release;
-
-    console.log(title);
+    const {
+      mintEndDateTime,
+      mintStartDateTime,
+      title,
+      releaseType,
+      artistName,
+      credits,
+      description,
+    } = release;
 
     const createdRelease = await prisma.release.create({
       data: {
         ownerId: "44bd6780-4ea5-431e-9ce4-1175888cd28e",
+        artistName,
         mintEndDateTime,
         mintStartDateTime,
         releaseType,
         title,
+        credits,
+        description,
       },
     });
 
